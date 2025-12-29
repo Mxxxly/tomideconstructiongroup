4dca6456c30b95bde0ad82320a9e3b8bc6e45e51
mac <mac@Mellys-Mac.local>
2025-12-29 04:44:19 -0800

    slight corrections on frontend

--- tcg/route.py
+++ tcg/route.py
@@ -8,18 +8,51 @@ from tcg.models import db,ServiceCategory,ServiceSubcategory,ContactUs
 from tcg.form import QuoteForm,ContactForm
 
 
+# @app.route('/')
+# def home_page():
+#     form = QuoteForm()
+
+#     # Load categories
+#     categories = ServiceCategory.query.all()
+#     form.category.choices =  [(c.id, c.name) for c in categories]
+
+#     # Leave subcategory empty until a category is selected
+#     form.subcategory.choices = []
+
+#     return render_template('users/index.html', form=form)
+
+
 @app.route('/')
 def home_page():
     form = QuoteForm()
 
     # Load categories
     categories = ServiceCategory.query.all()
-    form.category.choices =  [(c.id, c.name) for c in categories]
+    form.category.choices = [(c.id, c.name) for c in categories]
 
-    # Leave subcategory empty until a category is selected
+    # Default: empty subcategory choices
     form.subcategory.choices = []
 
-    return render_template('users/index.html', form=form)
+    # Check if category/subcategory are in query params
+    category_id = request.args.get('category', type=int)
+    subcategory_id = request.args.get('subcategory', type=int)
+
+    selected_category = None
+    selected_subcategory = None
+
+    if category_id:
+        selected_category = ServiceCategory.query.get(category_id)
+        if selected_category:
+            form.category.data = selected_category.id
+            # Populate subcategories for this category
+            form.subcategory.choices = [(s.id, s.name) for s in selected_category.subcategories]
+
+    if subcategory_id:
+        selected_subcategory = ServiceSubcategory.query.get(subcategory_id)
+        if selected_subcategory:
+            form.subcategory.data = selected_subcategory.id
+
+    return render_template('users/index.html', form=form, selected_category=selected_category, selected_subcategory=selected_subcategory, categories=categories)
 
 
 @app.route('/get_subcategories/<int:category_id>')
@@ -29,15 +62,21 @@ def get_subcategories(category_id):
 
 @app.route('/about/')
 def about():
-    return render_template('users/about.html')
+    categories = ServiceCategory.query.all()
+
+    return render_template('users/about.html',categories=categories)
 
 @app.route('/services/')
 def services():
-    return render_template('users/services.html')
+    categories = ServiceCategory.query.all()
+
+    return render_template('users/services.html',categories=categories)
 
 @app.route('/projects/')
 def projects():
-    return render_template('users/projects.html')
+    categories = ServiceCategory.query.all()
+
+    return render_template('users/projects.html',categories=categories)
 
 # @app.route('/blog/')
 # def blog():
@@ -47,6 +86,8 @@ def projects():
 @app.route('/contact/', methods=['GET','POST'])
 def contact():
     contact= ContactForm()
+    categories = ServiceCategory.query.all()
+
     if request.method == 'POST':
         if contact.validate_on_submit():
             name= contact.name.data
@@ -88,4 +129,4 @@ def contact():
         else:
             flash('Please correct the errors in the form.', 'danger ')
 
-    return render_template('users/contact.html',contact=contact)
+    return render_template('users/contact.html',contact=contact,categories=categories)
